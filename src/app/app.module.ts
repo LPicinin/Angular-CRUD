d6c1568f09aca4138fc22043c6ab56429a7c98a5
Luís Henrique Picinin Jandre <luishpj@outlook.com.br>
2024-09-13 00:38:31 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
+import { FormsModule } from '@angular/forms';
+import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
+import { PensamentoComponent } from './components/pensamentos/pensamento/pensamento.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ExcluirPensamentoComponent } from './components/pensamentos/excluir-pensamento/excluir-pensamento.component';
+import { EditarPensamentoComponent } from './components/pensamentos/editar-pensamento/editar-pensamento.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RodapeComponent,
+    CabecalhoComponent,
+    CriarPensamentoComponent,
+    ListarPensamentoComponent,
+    PensamentoComponent,
+    ExcluirPensamentoComponent,
+    EditarPensamentoComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { Pensamento } from '../pensamento';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrl: './pensamento.component.css',
+})
+export class PensamentoComponent {
+  @Input() pensamento : Pensamento = {
+    conteudo: '',
+    autoria: '',
+    modelo: '',
+  };
+
+  larguraPensamento(): string {
+    if(this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g'
+    }
+    return 'pensamento-p'
+  }
+}
